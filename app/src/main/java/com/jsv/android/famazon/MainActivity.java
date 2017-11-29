9cd3bc13f82a7593fccb9a15bf510e8c5045b6eb
Jonathan SARDO VISCUGLIA <jonathan.sardoviscuglia@gmail.com>
2017-11-29 10:18:11 +0100

    Tests menu gauche

--- app/src/main/java/com/jsv/android/famazon/MainActivity.java
+++ app/src/main/java/com/jsv/android/famazon/MainActivity.java
@@ -2,12 +2,22 @@ package com.jsv.android.famazon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] drawerItemList;
+    private ListView myDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerItemList = getResources().getStringArray(R.array.items);
+        myDrawer = (ListView) findViewById(R.id.my_drawer);
+        myDrawer.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_item, drawerItemList));
     }
 }
